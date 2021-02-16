6023e48965f5ccf28566533132a7d618705b88c1
Saurabh <agarwal@cs.wisc.edu>
2021-02-16 03:58:32 -0600

    moved to running powersgd

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ echo "$num_workers";
 echo $s3_prefix;
 #./run.sh -arch resnet18 -master-ip tcp://127.0.0.1:2345 -rank 0 -reducer powersgd -bsize 28 -dataset-location /home/ubunut -device cuda:0 -log-file temp -reducer powersgd -reducer-param 2
 source activate pytorch_p36
-python main_ddp.py --arch $arch --master-ip $master_ip --rank $rank --reducer $reducer --batch-size $bsize --dataset-location $dataset_location --device $device --log-file $log_file --reducer-param $reducer_param --num-workers $num_workers --s3-prefix $s3_prefix
+python main.py --arch $arch --master-ip $master_ip --rank $rank --reducer $reducer --batch-size $bsize --dataset-location $dataset_location --device $device --log-file $log_file --reducer-param $reducer_param --num-workers $num_workers --s3-prefix $s3_prefix
 
 
 # python main.py --arch $arch --master-ip $2 --rank $3 --reducer $4 --batch-size $5 --dataset-location $6 --device cuda:1 --log-file $8 --reducer $9 --reducer-param $reducer_param
