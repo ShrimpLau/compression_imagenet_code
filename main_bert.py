d0718eb848b6becf2b412de5cdc71f7342ebce83
Saurabh <thethreadpool@gmail.com>
2021-02-18 22:03:44 -0600

    Added code for bert

--- main_bert.py
+++ main_bert.py
@@ -700,9 +700,9 @@ def main_bert(args):
     global_rank = args.node_rank * 4 + args.local_rank
 
     data_dir = "/home/ubuntu/bert_data/Sogou_data"
-    bert_config_file = "/home/ubuntu/bert_data/data/chinese_L-12_H-768_A-12/bert_config.json"
+    bert_config_file = "/home/ubuntu/bert_data/chinese_L-12_H-768_A-12/bert_config.json"
     task_name = "sogou"
-    vocab_file = "/home/ubuntu/bert_data/data/chinese_L-12_H-768_A-12/vocab.txt"
+    vocab_file = "/home/ubuntu/bert_data/chinese_L-12_H-768_A-12/vocab.txt"
     do_lower_case = True
     max_seq_length = args.max_seq_length
     train_batch_size = args.batch_size
