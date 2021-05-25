539697b6b6e0328166c39d17c1168549c49593b9
Saurabh <thethreadpool@gmail.com>
2021-05-25 03:26:15 -0500

    Fixed nproc per node

--- run_ddp_var_bandwidth.sh
+++ run_ddp_var_bandwidth.sh
@@ -17,14 +17,14 @@ echo $s3_prefix;
 #./run.sh -arch resnet18 -master-ip tcp://127.0.0.1:2345 -rank 0 -reducer powersgd -bsize 28 -dataset-location /home/ubunut -device cuda:0 -log-file temp -reducer powersgd -reducer-param 2
 source activate pytorch_latest_p37
 sudo tc qdisc add dev ens3 root tbf rate 1gbit latency 50ms burst 10MB
-OMP_NUM_THREADS=8 python -m torch.distributed.launch --nproc_per_node=4 --nnodes=$num_workers --node_rank=$rank --master_addr=$master_ip --master_port=2345 main_bert.py --batch-size $bsize --dataset-location $dataset_location --log-file $log_file --s3-prefix "${s3_prefix}_1gbps" --node_rank $rank
+OMP_NUM_THREADS=8 python -m torch.distributed.launch --nproc_per_node=8 --nnodes=$num_workers --node_rank=$rank --master_addr=$master_ip --master_port=2345 main_bert.py --batch-size $bsize --dataset-location $dataset_location --log-file $log_file --s3-prefix "${s3_prefix}_1gbps" --node_rank $rank
 sudo tc qdisc del dev ens3 root tbf rate 1gbit latency 50ms burst 10MB
 sudo tc qdisc add dev ens3 root tbf rate 2gbit latency 50ms burst 10MB
-OMP_NUM_THREADS=8 python -m torch.distributed.launch --nproc_per_node=4 --nnodes=$num_workers --node_rank=$rank --master_addr=$master_ip --master_port=2345 main_bert.py --batch-size $bsize --dataset-location $dataset_location --log-file $log_file --s3-prefix "${s3_prefix}_2gbps" --node_rank $rank
+OMP_NUM_THREADS=8 python -m torch.distributed.launch --nproc_per_node=8 --nnodes=$num_workers --node_rank=$rank --master_addr=$master_ip --master_port=2345 main_bert.py --batch-size $bsize --dataset-location $dataset_location --log-file $log_file --s3-prefix "${s3_prefix}_2gbps" --node_rank $rank
 sudo tc qdisc del dev ens3 root tbf rate 2gbit latency 50ms burst 10MB
 sudo tc qdisc add dev ens3 root tbf rate 4gbit latency 50ms burst 10MB
-OMP_NUM_THREADS=8 python -m torch.distributed.launch --nproc_per_node=4 --nnodes=$num_workers --node_rank=$rank --master_addr=$master_ip --master_port=2345 main_bert.py --batch-size $bsize --dataset-location $dataset_location --log-file $log_file --s3-prefix "${s3_prefix}_4gbps" --node_rank $rank
+OMP_NUM_THREADS=8 python -m torch.distributed.launch --nproc_per_node=8 --nnodes=$num_workers --node_rank=$rank --master_addr=$master_ip --master_port=2345 main_bert.py --batch-size $bsize --dataset-location $dataset_location --log-file $log_file --s3-prefix "${s3_prefix}_4gbps" --node_rank $rank
 sudo tc qdisc del dev ens3 root tbf rate 4gbit latency 50ms burst 10MB
 sudo tc qdisc add dev ens3 root tbf rate 8gbit latency 50ms burst 10MB
-OMP_NUM_THREADS=8 python -m torch.distributed.launch --nproc_per_node=4 --nnodes=$num_workers --node_rank=$rank --master_addr=$master_ip --master_port=2345 main_bert.py --batch-size $bsize --dataset-location $dataset_location --log-file $log_file --s3-prefix "${s3_prefix}_8gbps" --node_rank $rank
+OMP_NUM_THREADS=8 python -m torch.distributed.launch --nproc_per_node=8 --nnodes=$num_workers --node_rank=$rank --master_addr=$master_ip --master_port=2345 main_bert.py --batch-size $bsize --dataset-location $dataset_location --log-file $log_file --s3-prefix "${s3_prefix}_8gbps" --node_rank $rank
 # python main.py --arch $arch --master-ip $2 --rank $3 --reducer $4 --batch-size $5 --dataset-location $6 --device cuda:1 --log-file $8 --reducer $9 --reducer-param $reducer_param
