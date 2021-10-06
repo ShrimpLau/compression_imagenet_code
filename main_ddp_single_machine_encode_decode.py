af686f491390ce0dcc76c954d21991102d4b5a54
Saurabh <thethreadpool@gmail.com>
2021-10-06 15:51:52 -0500

    Added grad reducers grace

--- main_ddp_single_machine_encode_decode.py
+++ main_ddp_single_machine_encode_decode.py
@@ -18,7 +18,7 @@ import torchvision.models as models
 from torch.autograd import Variable
 
 import gradient_reducers
-import grad_reducers
+import grad_reducers_grace
 import s3_utils
 from timer import Timer
 
